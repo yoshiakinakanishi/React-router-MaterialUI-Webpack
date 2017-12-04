b368cc8bc702775eab55d28160d71f46d136d3ab
DESKTOP-UOEV9JD\kaophooh <nakanishiyoshiaki0915@gmail.com>
2017-12-04 14:56:30 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -9,64 +9,45 @@ import {List, ListItem} from 'material-ui/List';
 import Checkbox from 'material-ui/Checkbox';
 import TimePicker from 'material-ui/TimePicker';
 
-
 import ABOUT1 from './ABOUT1';
 import ABOUT2 from './ABOUT2';
 import ABOUT3 from './ABOUT3';
 import ABOUT4 from './ABOUT4';
 
-
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
-
 
 const Left = () => (
   <MuiThemeProvider>
-    <Router>
-      <div>
-        <Link to="/" ><FlatButton label="TEXT1" fullWidth={true} /></Link><br /><br />
-        <Link to='/trans2' ><FlatButton label="TEXT2" fullWidth={true} /></Link><br /><br />
-        <Link to='/trans3' ><FlatButton label="TEXT3" fullWidth={true} /></Link><br /><br />
-        <Link to='/trans4' ><FlatButton label="TEXT4" fullWidth={true} /></Link><br /><br />        
-      </div>
-    </Router>
+      <ul className="list">
+        <li><Link to="/" ><FlatButton label="TEXT1" fullWidth={true} /></Link></li>
+        <li><Link to="/2" ><FlatButton label="TEXT2" fullWidth={true} /></Link></li>
+        <li><Link to="/3" ><FlatButton label="TEXT3" fullWidth={true} /></Link></li>
+        <li><Link to="/4" ><FlatButton label="TEXT4" fullWidth={true} /></Link></li>      
+      </ul>
   </MuiThemeProvider>
 );
-
-
-const Contents = () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={ABOUT1}/>
-      <Route path="/trans2" component={ABOUT2}/>
-      <Route path="/trans3" component={ABOUT3}/>
-      <Route path="/trans4" component={ABOUT4}/>
-    </div>
-  </Router>
-);
-
 
 class App extends Component {
   render() {
     return (
-      <div className="wrap">
-
+      <BrowserRouter>
+        <div className="wrap">
           <div　className="topNav"></div>
 
           <div className="leftNav">
             <div className="leftNav__hero">App</div>
-
-
             <Left />
-          
-
-
-
           </div>
 
-          <Contents />
-
-      </div>
+          <div>
+            <Route exact path="/" component={ABOUT1} />
+            <Route path="/2" component={ABOUT2} />
+            <Route path="/3" component={ABOUT3} />
+            <Route path="/4" component={ABOUT4} />
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
